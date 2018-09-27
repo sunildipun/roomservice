467e505b36a95892d20f68166bd845a15811cac7
Sunil <sunil.6318ie@gmail.com>
2018-09-28 01:46:55 +0530

    define fake db

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,116 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   rooms : any;
+  light: boolean =false;
+  climate:boolean = false;
+  music: boolean = false;
+  video:boolean = false;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.initializeRoom();
   }
 
   initializeRoom(){
-    
+    this.rooms = [
+      {
+        name: "Ground Floor",
+        featuredRoom:[
+          {
+            name: "Living Room",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "Formal Dining",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "Informal Dining",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          }
+        ]
+      },
+      {
+        name: "First Floor",
+        featuredRoom:[
+          {
+            name: "Family Lounge",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "Bedroom",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "Library",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          }
+        ]
+      },
+      {
+        name: "Master Ensuite",
+        featuredRoom:[
+          {
+            name: "Lounge",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "Bedroom",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          },
+          {
+            name: "His Dressroom",
+            activity:[
+              this.light,
+              this.climate,
+              this.music,
+              this.video
+            ]
+          }
+        ]
+      }
+    ]
   }
 
   goToRoom(){
