48d7690d841b411377a630e7242ecb604a2a5374
Sunil <sunil.6318ie@gmail.com>
2018-09-28 12:15:27 +0530

    dialog box added

---
+++ src/app/my-dialog/my-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit,Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-my-dialog',
+  templateUrl: './my-dialog.component.html',
+  styleUrls: ['./my-dialog.component.css']
+})
+export class MyDialogComponent implements OnInit {
+
+  modalTitle: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { 
+    this.modalTitle = data.title;
+    console.log(data)
+
+   }
+
+  ngOnInit() {
+  }
+
+}
